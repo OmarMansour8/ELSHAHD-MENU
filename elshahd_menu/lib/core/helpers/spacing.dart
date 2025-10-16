423379d158a7a84fec6c67c3bc7d966981041c7d
Omar Mansour <102729790+OmarMansour8@users.noreply.github.com>
2025-10-16 14:30:21 +0300

    - getting started

--- elshahd_menu/lib/core/helpers/spacing.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-SizedBox VerticalSpacing(double height) => SizedBox(height: height.h);
-
-SizedBox HorizontalSpacing(double width) => SizedBox(width: width.w);
